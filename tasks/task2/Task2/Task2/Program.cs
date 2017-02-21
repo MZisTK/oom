d45cbbe6efded6c7a48ed2a9a448b1f3869f2225
MZisTK <ic16b050@technikum-wien.at>
2017-02-21 20:15:25 +0100

    Autobeispiel Klassen Methoden Instanzen

--- tasks/task2/Task2/Task2/Program.cs
+++ tasks/task2/Task2/Task2/Program.cs
@@ -9,17 +9,30 @@ namespace Task2
 
     class Auto
     {
-        string Marke;
-        double Preis;
+       private string Marke;
+       private double Preis;
+
+        public Auto(string dieMarke, double derPreis)
+        {
+            if (dieMarke == null || dieMarke.Length < 1)
+                throw new Exception("Keine Marke");
+            Marke = dieMarke;
+            if (derPreis < 0)
+                throw new Exception("negativer Preis");
+            Preis = derPreis;
+        }
+
+        public string GetMarke() { return Marke; }
+        public double GetPreis() { return Preis; }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Auto Mazda = new Auto();
-            Auto Audi = new Auto();
-
-
+            Auto eins = new Auto("Mazda", 20000);
+            Auto zwei = new Auto("audi", 30000.5);
+            Console.WriteLine($"Auto: { eins.GetMarke()} {eins.GetPreis()}");
+            Console.WriteLine($"Auto: {zwei.GetMarke()} {zwei.GetPreis()}");
         }
     }
 }
