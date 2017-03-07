b46d28ab3b1d0bde69897f9a73bd31ccef44d7b5
MZisTK <ic16b050@technikum-wien.at>
2017-03-07 19:54:16 +0100

    Signed-off-by: MZisTK <ic16b050@technikum-wien.at>

---
+++ tasks/task3/task3/task3/task3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Program
+    {
+        static double MalZwei (double x)
+        {
+            return 2 * x;
+        }
+
+
+        static double apply(Func<double,double>f, double x)
+        {
+            return f(x);
+
+        }
+         
+        static void Main(string[] args)
+        {
+            Func<double, double> f = MalZwei;
+            Func<string, int, bool, byte> g;
+             
+            Console.WriteLine(f(1.0));
+            Console.WriteLine(apply(f,1.0));
+        }
+    }
+}
