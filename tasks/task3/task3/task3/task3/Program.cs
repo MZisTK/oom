c642cfe0a2a8f0f9aa1ec906a0d30384be934769
MZisTK <ic16b050@technikum-wien.at>
2017-03-07 20:04:15 +0100

    Signed-off-by: MZisTK <ic16b050@technikum-wien.at>

--- tasks/task3/task3/task3/task3/Program.cs
+++ tasks/task3/task3/task3/task3/Program.cs
@@ -8,25 +8,26 @@ namespace task3
 {
     class Program
     {
-        static double MalZwei (double x)
-        {
-            return 2 * x;
-        }
+       
 
 
         static double apply(Func<double,double>f, double x)
         {
             return f(x);
-
         }
          
         static void Main(string[] args)
         {
-            Func<double, double> f = MalZwei;
+            /*
+            Func<double, double> f =  x => 2 * x; 
             Func<string, int, bool, byte> g;
-             
-            Console.WriteLine(f(1.0));
-            Console.WriteLine(apply(f,1.0));
+            Console.WriteLine(f(1.0)); 
+            Console.WriteLine(apply(f,1.0));*/
+
+            var xs = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //xs ist ein Kunden array -> verdoppeln
+
+
         }
     }
 }
